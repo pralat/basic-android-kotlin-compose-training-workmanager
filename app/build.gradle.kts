ce50df4adf68b296cea1f9908c815a7e41e5dfb8
Mark Pralat <mpralat@gmail.com>
2026-04-28 11:42:19 -0400

    Done step 8 Create the BlurWorker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("androidx.work:work-runtime-ktx:2.11.2")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -1,4 +1,51 @@
 package com.example.bluromatic.workers
 
-class BlurWorker {
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.example.bluromatic.DELAY_TIME_MILLIS
+import kotlinx.coroutines.delay
+import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+private const val TAG = "BlurWorker"
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+    override suspend fun doWork(): Result {
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.blurring_image),
+            applicationContext
+        )
+        return withContext(Dispatchers.IO) {
+
+            return@withContext try {
+                // This is an utility function added to emulate slower work.
+                delay(DELAY_TIME_MILLIS)
+
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+                val output = blurBitmap(picture, 1)
+                // Write bitmap to a temp file
+                val outputUri = writeBitmapToFile(applicationContext, output)
+                makeStatusNotification(
+                    "Output is $outputUri",
+                    applicationContext
+                )
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
+        }
+
+    }
 }
